e711dc9b3ac7369a86c0799488b64ad4055fe7d4
abaschen <2025792+abaschen@users.noreply.github.com>
2024-04-17 09:18:33 +0000

    remove old account ids

--- bin/general-immersion-day-cdk.ts
+++ bin/general-immersion-day-cdk.ts
@@ -15,7 +15,7 @@ new GeneralImmersionDayCdkStack(app, 'GeneralImmersionDayCdkStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '468505660870', region: 'us-east-1' },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
